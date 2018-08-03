b4c30cd394014188616ab07625880beca41d4a6c
iGherx <itsmegherx@gmail.com>
2018-08-03 23:25:47 +0800

    Removed All

--- index.php
+++ index.php
@@ -1,12 +1,1 @@
-<?php echo "THIS IS TEST REPO 101" ?>
-
-<h1>When life gives you lemon, make a lemonade</h1>
-<?php echo "Wazzap nigga this is THUG LIFE!!!" ?>
-
-
-<?php echo "Wazzap nigga this is THUG LIFE!!!" ?>
-<<<<<<< HEAD
-
-<h1>Test Merge</h1>
-=======
->>>>>>> 97ae5b6e7a55c541830f23d6f892a83bcbb1aded
+<H1>REMOVE ALL</>
